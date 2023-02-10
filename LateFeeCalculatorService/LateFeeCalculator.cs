d386ce67796c9cff5c85951c58425f57c05f6614
veenaviju <veenaviju@gmail.com>
2023-02-10 15:03:42 +0530

    Basic logic implementation, no tests

--- LateFeeCalculatorService/LateFeeCalculator.cs
+++ LateFeeCalculatorService/LateFeeCalculator.cs
@@ -2,10 +2,34 @@
 {
     public class LateFeeCalculator
     {
-        public decimal Calculate(decimal amount, DateTime dueDate) 
+        public decimal Calculate(decimal BillAmount, DateTime dueDate)
         {
-            decimal feeAmount = 0;
-            return feeAmount; 
+
+            int NoOfDays = (DateTime.Now - dueDate).Days;
+
+            decimal calculatedLateFees = 0.00M;
+
+            if (NoOfDays >= 2 && NoOfDays < 5)
+            {
+                calculatedLateFees = BillAmount * 0.01M;
+            }
+            else if (NoOfDays >= 5 && NoOfDays < 10)
+            {
+                calculatedLateFees = BillAmount * 0.025M;
+            }
+            else if (NoOfDays >= 10 && NoOfDays < 15)
+            {
+                calculatedLateFees = BillAmount * 0.05M;
+            }
+            else if (NoOfDays >= 15)
+            {
+                calculatedLateFees = BillAmount * 0.1M;
+            }
+
+            if (calculatedLateFees <= 500.00M)
+                return calculatedLateFees;
+            else
+                return 500.00M;
         }
     }
 }
